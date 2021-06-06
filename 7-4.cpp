6fcc641dd12da88a00cb3085b6389ed0295c0443
DestaAri1 <72483038+DestaAri1@users.noreply.github.com>
2021-06-06 22:38:14 +0700

    Create 7-4.cpp

---
+++ 7-4.cpp
@@ -0,0 +1,9 @@
+/* Desta Ari Alfananda */
+/* 20051397008 */
+/* 2020B */
+
+/*Kesimpulan : Pengurutan data (sorting) didefinisikan sebagai suatu proses untuk menyusun kembali humpunan obyek menggunakan aturan tertentu. 
+Metode penyisipan (Insertion sort) bertujuan untuk menjadikan bagian sisi kiri array terurutkan sampai dengan seluruh array berhasil diurutkan. 
+Metode ini mengurutkan bilangan-bilangan yang telah dibaca; dan berikutnya secara berulang akan menyisipkan bilangan-bilangan dalam array yang 
+belum terbaca ke sisi kiri array yang telah terurut. Lalu untuk cara kerja algoritma selection sort adalah pada elemen acuan pada indeks ke-i, 
+akan memilih elemen dengan nilai paling kecil pada indeks berikutnya (ke-j=i+1 s/d ke-n) dan menukar elemen acuan dengan elemen terkecil.*/
